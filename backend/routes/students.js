7a1ee3edfd509cfc3b528a9b9109dcd74a299a70
Aysan Ebrahimi <aysanebrahimi1998@gmail.com>
2024-11-25 01:44:31 +0330

    Authentication for POST,PUT and DELETE routes is implemented in students

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,16 +1,24 @@
-import express from 'express'
+import express from "express";
 import {
-    createStudent,
-    getStudents,
-    updateStudent,
-    deleteStudent,
-    getStudentDetails
-} from '../controllers/studentControllers.js';
+  createStudent,
+  getStudents,
+  updateStudent,
+  deleteStudent,
+  getStudentDetails,
+} from "../controllers/studentControllers.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+
 const router = express.Router();
 
-router.route("/student").post(createStudent);
+router
+  .route("/admin/student")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createStudent);
 router.route("/students").get(getStudents);
-router.route("/student/:id").put(updateStudent);
-router.route("/student/:id").delete(deleteStudent);
+router
+  .route("/admin/student/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateStudent);
+router
+  .route("/admin/student/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteStudent);
 router.route("/student/:id").get(getStudentDetails);
 export default router;
