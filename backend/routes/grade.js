9594b30c60adf5956922a05c6ea8cd597401b4b9
Aysan Ebrahimi <aysanebrahimi1998@gmail.com>
2024-11-25 01:40:37 +0330

    Authentication for POST,PUT and DELETE routes is implemented in grade

--- backend/routes/grade.js
+++ backend/routes/grade.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import { authorizeRoles,isAuthenticatedUser } from "../middlewares/auth.js";
+import express from "express";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
+import {
+  createGrade,
+  getGrades,
+  updateGrade,
+  deleteGrade,
+  getGradeDetails,
+} from "../controllers/gradeControllers.js";
 
+router.route("/admin/grade").post(isAuthenticatedUser, createGrade);
 
+router.route("/grades").get(getGrades);
+router
+  .route("/admin/grade/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateGrade)
+  .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteGrade);
 
-import { createGrade, getGrades, updateGrade, deleteGrade, getGradeDetails } from '../controllers/gradeControllers.js';
-
-router.route('/grade').post(isAuthenticatedUser, createGrade);
-
-router.route('/grades').get(getGrades);
-router.route('/grade/:id').put(updateGrade).delete(deleteGrade);
-
-router.route('/grade/:id').get(getGradeDetails);
-
-
+router.route("/grade/:id").get(getGradeDetails);
 
 export default router;
