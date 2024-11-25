aa1ce826b1477731c617bd8ec925dd287604abea
Muhammad Zaryab Khan <zaryabkhan864@gmail.com>
2024-11-25 09:34:00 +0300

    rebasing issue

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -10,22 +10,6 @@ import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth";
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.route("/courses").post(newCourse);
-router.route("/courses").get(getCourses);
-router
-  .route("/admin/courses")
-  .post(isAuthenticated, authorizeRoles("admin"), newCourse);
-router
-  .route("/admin/courses/:id")
-  .get(isAuthenticated, authorizeRoles("admin"), getCourseDetails);
-router
-  .route("admin/courses/:id")
-  .put(isAuthenticated, authorizeRoles("admin"), updateCourse);
-router
-  .route("admin/courses/:id")
-  .delete(isAuthenticated, authorizeRoles("admin"), deleteCourse);
-=======
 router
   .route("/admin/courses")
   .post(isAuthenticatedUser, authorizeRoles("admin"), newCourse);
@@ -35,5 +19,4 @@ router.route("/admin/courses/:id").put(isAuthenticatedUser, authorizeRoles("admi
 router.route("/admin/courses/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteCourse);
 router.route("/courses/:id").get(getCourseDetails);
 
->>>>>>> 5d7da77 (Authentication for POST,PUT and DELETE routes is implemented in course)
 export default router;
