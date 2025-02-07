d0942ec623be54e93af8935a2ec0d5c3f77efd34
Maham Rahim <mahamrahim96@gmail.com>
2025-02-08 01:54:13 +0500

    fix

--- frontend/src/components/exam/AddExam.jsx
+++ frontend/src/components/exam/AddExam.jsx
@@ -64,6 +64,7 @@ const AddExam = () => {
             const body = {
                 gradeId: formValues.grade,
                 teacherId: userDetails.userId,
+                userRole: userDetails.role
             };
             sendGradeAndTeacherID(body)
                 .unwrap()
@@ -72,7 +73,7 @@ const AddExam = () => {
                 })
                 .catch((err) => console.error('Error fetching courses:', err));
         }
-    }, [formValues.grade, userDetails.userId, sendGradeAndTeacherID]);
+    }, [formValues.grade, userDetails.userId, sendGradeAndTeacherID, userDetails.role]);
 
 
      // Function to fetch quiz details
